d7e66a35a7bc3caee41eddd9bade69831f4e3337
srajankumar <kumarsrajan02@gmail.com>
2023-08-03 18:59:44 +0530

    navbar + darkmode

--- src/app/team/page.tsx
+++ src/app/team/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <div className="w-full bg-white dark:bg-green-400 h-screen"></div>
+      <div className="w-screen bg-white dark:bg-yellow-600 h-screen"></div>
     </div>
   );
 }
