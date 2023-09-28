2532aca1f3ec1718a80fb6dca6a72e9c645ac09b
srajan <kumarsrajan02@gmail.com>
2023-09-28 11:23:43 +0530

    metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,53 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Footer from "@/components/Footer";
+import { siteConfig } from "@/config/site";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "IEI - SCEM",
-  description: "Website of Sahyadri IEI CSE Student Chapter",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "iei",
+    "scem",
+    "Sahyadri",
+    "Sahyadri College of Engineering and Management",
+    "Sahyadri IEI",
+    "IEI",
+    "IEI-CSE",
+    "iei cse student chapter",
+    "sahyadri student chapter",
+  ],
+  creator: "srajankumar",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+    images: [
+      {
+        url: siteConfig.ogImage,
+        width: 1200,
+        height: 630,
+        alt: siteConfig.name,
+      },
+    ],
+  },
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
 };
 
 export default function RootLayout({
---
+++ src/config/site.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+  name: "IEI-SCEM",
+  url: "https://iei-scem.vercel.app/",
+  ogImage: "",
+  description:
+    "IEI Student Chapter, Department of Computer Science & Engineering, SAHYADRI College of Engineering and Management Mangaluru, established in the year 2021. The student chapter has been established with the vision of providing a professional networking Platform to the stakeholders, opportunity to Exhibit Technical and Engineering Talent through Project Competition, organizing technical events in association with a recognized professional body.",
+};
